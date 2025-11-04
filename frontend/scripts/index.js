e5416e395e537d646e548a3123446187873d22cd
heisck <kelvinkwabenaparkingston@gmail.com>
2025-11-04 07:28:22 +0000

    Add files via upload

---
+++ frontend/scripts/index.js
@@ -0,0 +1,16 @@
+function sendMsg() {
+  const send = document.querySelector('.send-button');
+  send.addEventListener('click', () => {
+    const input = document.querySelector('input');
+    const msg = input.value;
+    if (msg) {
+      const msgArea = document.querySelector('.message-sect');
+      msgArea.innerHTML += `<div class="user-message">${msg}</div>`
+      input.value = '';
+      msgArea.scrollTo({ top: msgArea.scrollHeight, behavior: 'smooth' });
+    }
+  }) 
+}
+
+sendMsg();
+
